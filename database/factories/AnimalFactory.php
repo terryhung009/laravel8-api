e051ef65a0fe5791a5db727598f86bf9e7d26a47
terryhung009 <khc741213@gmail.com>
2021-11-28 21:54:54 +0800

    factory 產生測試資料

--- database/factories/AnimalFactory.php
+++ database/factories/AnimalFactory.php
@@ -3,10 +3,15 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Animal;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class AnimalFactory extends Factory
 {
-    /**
+    protected $model = Animal::class;
+
+        /**
      * Define the model's default state.
      *
      * @return array
@@ -15,6 +20,17 @@ class AnimalFactory extends Factory
     {
         return [
             //
+            //numberBetween隨機產生範圍1到3之間的整數
+            'type_id' => $this->faker->numberBetween(1,3),
+            'name' => $this->faker->name,
+            'birthday' => $this->faker->date(),
+            'area' => $this->faker->city,
+            'fix' => $this->faker->boolean,
+            'description' => $this->faker->text,
+            'personality' => $this->faker->text,
+            'user_id' => User::all()->random()->id
         ];
     }
+
+    
 }
