37e0eec69a30a1587ace4586493d9ae833e8e715
Danny <khc741213@gmail.com>
2021-12-12 23:44:58 +0800

    update
    update animalController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\TypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('animals',AnimalController::class);
+
+Route::apiResource('types',TypeController::class);
